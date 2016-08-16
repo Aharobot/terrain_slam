7a14622a8e9929407a0c760a6239f216924ea815
Miquel Massot <miquel.massot@uib.cat>
2016-08-16 11:59:36 +0200

    prepare batch script

---
+++ scripts/run_tests.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import subprocess
+import numpy
+from sys import exit
+import re
+
+
+generate_hemisphere = ['python', 'generate_hemisphere.py']
+generate_step = ['python', 'generate_step.py']
+register_clouds = ['./../build/test_adjuster p1.pcd p2.pcd']
+
+args = []
+
+noises = [0.001, 0.01, 0.1, 0.5, 1.0, 1.5, 2.0]
+xs = [0.5, 1.0, 2.0, 4.0, 6.0]
+ys = [0.5, 1.0, 2.0, 4.0, 6.0]
+zs = [0, 0.5, 1.0]
+yaws = [0, 0.1, 0.2]
+
+cost = []
+convergence = []
+transform = numpy.zeros((1575, 4))
+f = open('workfile', 'w')
+idx = 0
+s =  'i convergence cost x y z yaw fx fy fz fyaw'
+f.write(s)
+for noise in noises:
+  for x in xs:
+    for y in ys:
+      for z in zs:
+        for yaw in yaws:
+          args.append(str(noise))
+          args.append(str(x))
+          args.append(str(y))
+          args.append(str(z))
+          args.append(str(yaw))
+          generate_clouds = generate_step + args
+          print generate_clouds
+          print 'Calling generate'
+          cmd = subprocess.call(generate_clouds)
+          print register_clouds
+          print 'Calling register'
+          cmd = subprocess.Popen(register_clouds, shell=True, stdout=subprocess.PIPE)
+          for line in cmd.stdout:
+            if re.search("CONVERGENCE", line):
+              if line[11] == '?':
+                convergence.append(int(line[13]))
+            if re.search("FINAL COST", line):
+              words = line.split()
+              cost.append(float(words[2]))
+            if re.search("After", line):
+              words = line.replace('(',' ').replace(')',' ').replace(',',' ').split()
+              transform[idx][0] = float(words[1])
+              transform[idx][1] = float(words[2])
+              transform[idx][2] = float(words[3])
+              transform[idx][3] = float(words[7])
+          print idx, convergence[idx], cost[idx], x, y, z, yaw, transform[idx][0], transform[idx][1], transform[idx][2], transform[idx][3]
+          s = idx, convergence[idx], cost[idx], x, y, z, yaw, transform[idx][0], transform[idx][1], transform[idx][2], transform[idx][3]
+          f.write(', '.join(str(c) for c in s))
+          f.write('\n')
+          idx = idx + 1
+f.close()
+
+
